f1f251d8cd5c7f2ee637bd64ad200b739782c91d
KKang <rkdalsdn0914@naver.com>
2025-02-20 14:30:18 +0900

    2025-02-20./14/30

--- 실습문제/2025-02-20/1226/미로1.py
+++ 실습문제/2025-02-20/1226/미로1.py
@@ -1,10 +1,40 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-T = int(input())
+T = 10
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
+    testca = int(input())
     arr = [list(input()) for _ in range(16)]
-    check = [[0] * 16 for _ in range(16)]
-    q = []
+
+
+    def bfs(i, j):
+        q = []
+        check = [[0] * 16 for _ in range(16)]
+        q.append([i, j])
+        check[i][j] = 1
+        flag = 0
+        # print(len(q))
+
+        while q:
+            i, j = q.pop(0)
+            if arr[i][j] == '3':
+                return 1
+            for dr in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+                ni = i + dr[0]
+                nj = j + dr[-1]
+                if -1 < ni < 16 and -1 < nj < 16 and arr[ni][nj] != '1' and check[ni][nj] != 1:
+                    q.append([ni, nj])
+                    check[ni][nj] = 1
+        return 0
+
+
+    i = 1
+    j = 1
+    result = bfs(i,j)
+
+    print(f'#{testca} {result}')
+
+
+
 
