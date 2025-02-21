edaac33189cf4caa1e50192597885f0ab27a4048
KKang <rkdalsdn0914@naver.com>
2025-02-21 17:54:37 +0900

    ssafy20250221

--- 실습문제/2025-02-19/queue/5099/pizza.py
+++ 실습문제/2025-02-19/queue/5099/pizza.py
@@ -15,21 +15,15 @@ for test_case in range(1, T + 1):
     count = N
 
     while len(fire) != 1 :
-        su = fire.popleft()
+        su = fire.popleft()                 # 핏짜 꺼내서 구워졌는지 확인
         su[0] //= 2
-        if su[0] == 0 :
+        if su[0] == 0 :                     #구워 졌다면
 
-            if count < M :
-                fire.appendleft([arr[count],count +1 ])
+            if count < M :                                  # 그리고 남은 핏짜가 있다면 남은 핏짜 넣어라
+                fire.append([arr[count],count +1 ])
                 count += 1
-                back = fire.pop()
-                fire.appendleft(back)
-            else:
-                back = fire.pop()
-                fire.appendleft(back)
 
         else:
-            fire.appendleft(su)
-            back = fire.pop()
-            fire.appendleft(back)
+            fire.append(su)                             #핏짜가 덜 구워졌다면 그 핏짜 다시 내려놔라
+
     print(f'#{test_case} {fire[0][-1]}')
