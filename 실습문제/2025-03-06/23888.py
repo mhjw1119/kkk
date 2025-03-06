513acc5cd58e580f9fd27d5180abbce1abc8493a
kkang <rkdalsdn0914@naver.com>
2025-03-06 12:42:47 +0900

    'clear23888'

--- 실습문제/2025-03-06/23888.py
+++ 실습문제/2025-03-06/23888.py
@@ -1,20 +1,31 @@
+import  sys
+sys.stdin = open('input.txt','r')
+
 def two( N , count, back, log ) :
-    if N == -1 :
+    if N == len(arr1) :
         return
 
-    if arr[N] == 1 :
+    if arr1[N] == '1' :
         back += 2 ** log
     if count == 6 :
-        result.append(back)
-        log = -1
+        result.append(str(back))
+        log = 7
         count = -1
         back = 0
-    two(N-1,count+1, back, log+1)
+    two(N+1,count+1, back, log-1)
 
 
-arr = list(map(int, input()))
-n = len(arr)-1
-result = []
-two(n,0,0,0)
-result = list(reversed(result))
-print(result)
+T = int(input())
+
+for testcase in range(1,T+1) :
+    N1 = int(input())
+    arr1 = ''
+    for i in range(N1) :
+        ai = str(input())
+        arr1 = arr1 + ai
+    # for i in range(N1) :
+    n = len(arr1)-1
+    result = []
+    two(0,0,0,0)
+    # result = list(reversed(result))
+    print(f'#{testcase} {" ".join(result)}')
