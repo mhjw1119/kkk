e1e7b69989eba47fa8fc6785fc74f166c902c138
kkang <rkdalsdn0914@naver.com>
2025-03-06 17:56:43 +0900

    'backup'

--- 실습문제/2025-03-06/23884/sachick.py
+++ 실습문제/2025-03-06/23884/sachick.py
@@ -1,6 +1,10 @@
-import sys
-sys.stdin = open('input.txt', 'r')
+# import sys
+# sys.stdin = open('input.txt', 'r')
 
-T = int(input())
+T = 10
 
 for testcase in range(1, T+ 1):
+    N = int(input())
+    arr = [[] for _ in range(N+1)]
+
+    if input() != int :
