0906d47384ad6d0de3c30826d65300ccc4dba616
kkang <rkdalsdn0914@naver.com>
2025-03-10 12:32:21 +0900

    '3499_clear'

--- 실습문제/2025-03-10/3499/퍼펙트 셔플.py
+++ 실습문제/2025-03-10/3499/퍼펙트 셔플.py
@@ -1,26 +1,12 @@
 '''
 오버핸드 셔플 :   덱
 int(0.37 * N)만큼 맨 뒤에서 카운트해서 앞으로 가져옴
-퍼펙트 셔플 :    스택
+퍼펙트 셔플 :    큐
 int(0.5 * N)만큼 맨 뒤에서 떼어내서 배열에 담아두고
 원래배열에 있는 넘들 큐에 하나 넣고 그 다음에 맨 뒤에서 떼어낸 배열 하나씩 넣는다.
 '''
-from collections import deque
-
-def overhand ( N1 ) :
-    over_b = int(0.37 * N1)
-    if over_b  == 0 :
-        return
-    global arr
-    over = deque()
-    for o in range(N1):
-        over.append(arr[o])
-
-    for _ in range(over_b) :
-        over.appendleft(over.pop())
-    arr = over
-
-
+import sys
+sys.stdin = open('input.txt','r')
 
 def perf ( N2 ) :
     global arr
@@ -44,17 +30,8 @@ def perf ( N2 ) :
 T = int(input())
 
 for tc in range(1,T+1) :
-    check = 1
-    arr_N, N = map(int, input().split())
-    arr = []
-    for i in  range(arr_N) :
-        arr.append(i+1)
+    N = int(input())
+    arr = list(map(str,input().split()))
+    perf(N)
 
-    for _ in range(N) :
-        overhand(arr_N)
-        perf(arr_N)
-
-    result = ''
-    for qq in arr :
-        result += str(qq)
-    print(f'#{tc} {" ".join(result)}')
+    print(f'#{tc} {" ".join(arr)}')
